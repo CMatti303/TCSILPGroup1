7af7edbbe9356ef57b635132131828e4035d4132
abali332 <94724131+abali332@users.noreply.github.com>
2021-12-16 10:19:07 -0800

    Update login.component.ts

--- Frontend/certproject/src/app/components/login/login.component.ts
+++ Frontend/certproject/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: any;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+  ) {
+      // redirect to home if already logged in
+      // if (this.authenticationService.currentUserValue) {
+      //     this.router.navigate(['/']);
+      // }
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+      this.loginForm = this.formBuilder.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
+
+      // get return url from route parameters or default to '/'
+      // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // reset alerts on submit
+      // this.alertService.clear();
+
+      // stop here if form is invalid
+      if (this.loginForm.invalid) {
+          return;
+      }
+
+      this.loading = true;
+
+      fetch("http://localhost:3000/login", {
+        method: "POST",
+        body: JSON.stringify({
+          email: this.f.username.value,
+          passoword: this.f.password.value,
+        })
+      })
+      // this.authenticationService.login(this.f.username.value, this.f.password.value)
+      //     .pipe(first())
+      //     .subscribe(
+      //         data => {
+      //             this.router.navigate([this.returnUrl]);
+      //         },
+      //         error => {
+      //             this.alertService.error(error);
+      //             this.loading = false;
+      //         });
   }
 
 }
